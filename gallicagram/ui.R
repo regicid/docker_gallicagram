e54347b1e307ea90d24b82b10115a61a16ec8386
benjyazoulay <benjyazoulay@gmail.com>
2021-04-13 20:41:22 +0200

    ahaha

--- gallicagram/ui.R
+++ gallicagram/ui.R
@@ -1,16 +1,5 @@
 library(shiny)
-library(ggplot2)
-library(plotly)
-library(stringr)
-library(Hmisc)
-library(xml2)
-library(markdown)
-library(shinythemes)
-library(htmlwidgets)
-library(httr)
-library(ngramr)
-library(dplyr)
-library(htmltools)
+
 
 shinyUI(
     fluidPage(
