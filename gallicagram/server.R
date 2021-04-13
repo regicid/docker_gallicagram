dd2699e0791536375cad999fefc6168982f76f5a
benjyazoulay <benjyazoulay@gmail.com>
2021-04-13 21:32:47 +0200

    ahaha

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -1,16 +1,4 @@
 library(shiny)
-library(ggplot2)
-library(plotly)
-library(stringr)
-library(Hmisc)
-library(xml2)
-library(markdown)
-library(shinythemes)
-library(htmlwidgets)
-library(httr)
-library(ngramr)
-library(dplyr)
-library(htmltools)
 
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
