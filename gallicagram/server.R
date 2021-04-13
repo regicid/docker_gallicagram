3b91ccab6c75058862b2f3f11e58224f05a98c58
benjyazoulay <benjyazoulay@gmail.com>
2021-04-13 22:06:31 +0200

    ahaha

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -4,7 +4,13 @@ library(plotly)
 library(stringr)
 library(Hmisc)
 library(xml2)
-
+library(markdown)
+library(shinythemes)
+library(htmlwidgets)
+library(httr)
+library(ngramr)
+library(dplyr)
+library(htmltools)
 
 shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
--- gallicagram/ui.R
+++ gallicagram/ui.R
@@ -1,4 +1,16 @@
 library(shiny)
+library(ggplot2)
+library(plotly)
+library(stringr)
+library(Hmisc)
+library(xml2)
+library(markdown)
+library(shinythemes)
+library(htmlwidgets)
+library(httr)
+library(ngramr)
+library(dplyr)
+library(htmltools)
 
 shinyUI(fluidPage(
     sidebarLayout(
