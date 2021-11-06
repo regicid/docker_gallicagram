e97029bc5d6ac6c1b25c900e2c1a2b503148d2b2
benjyazoulay <benjyazoulay@gmail.com>
2021-11-06 19:17:21 +0100

    graphique scientifique

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -293,7 +293,7 @@ SPlot <- function(data,input){
   
   else{plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot))+geom_point(data=tableau, aes(x = date, y = ratio, group=mot,color=mot),size=.5, alpha=.5) + geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+xlab("")+ylab("Fréquence dans le corpus")+
     geom_rangeframe() + theme_tufte()+ scale_color_manual(values=customPalette)+
-    theme(legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=5))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
+    theme(legend.title= element_blank(),axis.text.y = element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=5))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
   
   return(plot)
 }
@@ -2216,7 +2216,7 @@ shinyServer(function(input, output,session){
       paste('Splot_',input$mot,"_",input$beginning,"_",input$end,'.png', sep='')
     },
     content = function(filename) {
-      ggsave(filename,SPlot(data,input),width = 32, height = 18,units = "cm", dpi=300)
+      ggsave(filename,SPlot(data,input),width = 16, height = 9,units = "cm", dpi=300)
     })
   output$data_session <- downloadHandler(
     filename = function() {
