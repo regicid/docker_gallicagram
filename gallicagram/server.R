3e4afd86c592106bc11411b870563daff44c12b9
benjyazoulay <benjyazoulay@gmail.com>
2021-11-06 19:00:13 +0100

    graphique scientifique

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -289,11 +289,11 @@ SPlot <- function(data,input){
   
   if(input$scale==TRUE |input$multicourbes==TRUE){plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot))+ geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+xlab("")+ylab("")+
      theme_tufte()+ scale_color_manual(values=customPalette)+
-    theme(axis.line.y=element_blank(),axis.text.y = element_blank(),axis.ticks.y = element_blank(),legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=5))+guides(fill=guide_legend(nrow=3,ncol = 3,byrow=TRUE))}
+    theme(axis.line.y=element_blank(),axis.text.y = element_blank(),axis.ticks.y = element_blank(),legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=5))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
   
   else{plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot))+geom_point(data=tableau, aes(x = date, y = ratio, group=mot,color=mot),size=.5, alpha=.5) + geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+xlab("")+ylab("Fréquence dans le corpus")+
     geom_rangeframe() + theme_tufte()+ scale_color_manual(values=customPalette)+
-    theme(legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=5))+guides(fill=guide_legend(nrow=3,ncol = 3,byrow=TRUE))}
+    theme(legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=5))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
   
   return(plot)
 }
