6b87fa1e3e2cab82e2f1a2fe3ecaa4f9767343f7
benjyazoulay <benjyazoulay@gmail.com>
2022-01-01 23:35:53 +0100

    jokerizer

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -795,7 +795,7 @@ jokerize<-function(input){
   con=dbConnect(RSQLite::SQLite(),dbname = ngram_file)
   
   
-  query = dbSendQuery(con,str_c("select sum(n) as tot ",gram," from full_text WHERE ",gram," MATCH '^",mot,"' group by ",gram," order by tot desc limit ",input$nbJoker))
+  query = dbSendQuery(con,str_c('select sum(n) as tot, ',gram,' from ',gram,' where annee between ',input$beginning,' and ',input$end,' and rowid in (select rowid from full_text where ',gram,' match "^',mot,'") group by ',gram,' order by tot desc limit ',input$nbJoker,';'))
   w = dbFetch(query)
   print(w)
   
