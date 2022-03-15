2511141648da14a001aa897c2b47fca9314d15af
benjyazoulay <benjyazoulay@gmail.com>
2022-03-15 17:49:37 +0100

    ngram le monde jour

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -1042,11 +1042,10 @@ ngramize<-function(input,nouvrequette){
             base<-base%>%group_by(annee,mois)%>%summarise(n = sum(n))
             base<-cbind(str_c(base$annee,"/",base$mois),base$n)
             colnames(base)<-c("date","base")}
-          if(input$resolution=="Jours"){
-            print(2)
+          if(input$resolution=="Jour"){
             base<-cbind(str_c(base$annee,"/",base$mois,"/",base$jour),base$n)
             colnames(base)<-c("date","base")}
-          print(3)
+          print(base)
         }
         if(nb>2){z=data.frame(date=from:to, count=0, base=0,ratio=0)
         next}
