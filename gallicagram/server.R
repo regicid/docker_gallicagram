b3b4c115cbb9bcfd0c3d231c98e53d8b96bd885e
benjyazoulay <benjyazoulay@gmail.com>
2022-04-26 12:53:04 +0200

    mediacloud

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -2559,8 +2559,8 @@ get_data <- function(mot,from,to,resolution,doc_type,titres,input,cooccurrences,
     
     url<-str_c("https://api.mediacloud.org/api/v2/stories_public/count?q=",mot,"&fq=publish_date:%5B",jjj,"T00:00:00.000Z+TO+",kkk,"T00:00:00.000Z%5D&language=",lang,"&split=T&split_period=",res,"&key=b2ef1a99a8fdbb84afafe742fd437c0942703072bb98242ba0eed9b8411e1735")
     url_base<-str_c("https://api.mediacloud.org/api/v2/stories_public/count?q=a&fq=publish_date:%5B",jjj,"T00:00:00.000Z+TO+",kkk,"T00:00:00.000Z%5D&language=",lang,"&split=T&split_period=",res,"&key=b2ef1a99a8fdbb84afafe742fd437c0942703072bb98242ba0eed9b8411e1735")
-    url=URLencode(url)
-    url_base=URLencode(url_base)
+    url=str_replace(url," ","%20")
+    url_base=str_replace(url_base," ","%20")
     print("---")
     a<-as.data.frame(fromJSON(url))
     b<-as.data.frame(fromJSON(url_base))
