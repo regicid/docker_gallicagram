8253c863dbd142db29a119a76c2d68329a8e7843
benjyazoulay <benjyazoulay@gmail.com>
2022-12-19 19:46:07 +0100

    séparation courbe corpus gallicagram

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -384,7 +384,7 @@ SPlot <- function(data,input){
     customPalette = customPalette[c(2,1)]
   }
   
-  print(tableau$loess)
+  print(tableau)
   if(input$histogramme==T){
     plot=ggplot(data=tableau, aes(x = date, y = count, group=mot,fill=mot))+geom_bar(stat="identity",position=position_dodge())+xlab("")+ylab("")+
       theme_tufte()+ scale_fill_manual(values=customPalette)+
@@ -396,8 +396,8 @@ SPlot <- function(data,input){
         theme_tufte()+ scale_color_manual(values=customPalette)+
         theme(plot.background = element_rect(fill = 'white', colour = 'white'),axis.line.x = element_line(colour = "black"),axis.text.y = element_blank(),axis.ticks.y = element_blank(),legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=6),legend.justification="left", legend.margin=margin(0,0,0,0),legend.box.margin=margin(-10,-10,0,-10),legend.key.height = unit(.5, 'lines'))+guides(color=guide_legend(nrow=2, byrow=TRUE))
     }
-    else if (input$span==0 | input$points==F | input$doc_type==0){
-      if (input$spline==T){plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot)) + geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+xlab("")+ylab("")+
+    else if (input$span==0 | input$points==F){
+      if (input$spline==T & input$doc_type!=0){plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot)) + geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+xlab("")+ylab("")+
         theme_tufte()+ scale_color_manual(values=customPalette)+
         theme(plot.background = element_rect(fill = 'white', colour = 'white'),axis.line.x = element_line(colour = "black"),axis.line.y = element_line(colour = "black"),legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=8),legend.justification="left", legend.margin=margin(0,0,0,0),legend.box.margin=margin(-10,-10,0,-10),legend.key.height = unit(.5, 'lines'))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
       else{plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot)) + geom_line(aes(color=mot),size=.7)+xlab("")+ylab("")+
