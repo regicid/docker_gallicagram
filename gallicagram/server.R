e980d5d9a80af61053aae93058ffd74dd9fbc25d
benjyazoulay <benjyazoulay@gmail.com>
2022-12-21 17:30:56 +0100

    reorder

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -193,7 +193,7 @@ Plot <- function(data,input){
     total$hovers<-str_c(total$mot," : ",total$hovers)
     total<-total%>%group_by(mot)
     plot<-plot_ly(x=~total$date,y=reorder(total$mot, total$count, sum),type = 'scatter', mode = 'markers',customdata=total$url, color=~total$mot,colors=customPalette,size = ~total$ratio,sizes = c(0, 50),marker = list( sizemode = "diameter", opacity = 0.3),text=~total$hovers,hoverinfo="text")
-    if(length(unique(tableau$mot))>9){plot<-plot_ly(x=~total$date,y=total$mot,type = 'scatter', mode = 'markers',customdata=total$url, color=~total$mot,size = ~total$ratio,sizes = c(0, 50),marker = list( sizemode = "diameter", opacity = 0.3),text=~total$hovers,hoverinfo="text")}
+    if(length(unique(tableau$mot))>9){plot<-plot_ly(x=~total$date,y=reorder(total$mot, total$count, sum),type = 'scatter', mode = 'markers',customdata=total$url, color=~total$mot,size = ~total$ratio,sizes = c(0, 50),marker = list( sizemode = "diameter", opacity = 0.3),text=~total$hovers,hoverinfo="text")}
     plot<-layout(plot,xaxis=list(title=""))
     plot = layout(plot,showlegend=F)
     return(onRender(plot,js))
