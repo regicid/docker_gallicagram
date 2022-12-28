f664ea6dac917124494cc2800570eb700134dbf9
benjyazoulay <benjyazoulay@gmail.com>
2022-12-28 19:58:59 +0100

    afc line fit

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -30,6 +30,7 @@ library(timetk)
 library(jsonlite)
 library(ggwordcloud)
 library(shinyalert)
+library(bezier)
 
 httr::set_config(config(ssl_verifypeer = 0L))
 
@@ -102,6 +103,12 @@ Plot <- function(data,input){
     if( input$resolution=="Mois"){b = str_extract(b,"....")
     }
     bb<-fviz_pca_biplot(res.pca,geom.var = c("text"),geom.ind = c("text"), label="all",labelsize=3,col.ind=as.integer(b),col.var="black")+labs(title="") + scale_color_gradientn(colors=rainbow(10,start=.65),guide="none")
+    
+    gg=as.data.frame(cbind(res.pca$ind$coord[,1],res.pca$ind$coord[,2]))
+    colnames(gg)=c("x","y")
+    gg=as.data.frame(bezier::bezier(seq(0, 1, len=100), gg, deg=nrow(gg)-1))
+    colnames(gg)=c("x","y")
+    bb=bb+geom_path(data=gg,aes(x,y),col=1)
     plot=ggplotly(bb)
     return(onRender(plot,js))
     }
@@ -470,7 +477,7 @@ SPlot <- function(data,input){
     if( input$resolution=="Mois"){
   b = str_extract(b,"....")}
     repel = (length(unique(tableau$date))<30)
-    plot<-fviz_pca_biplot(res.pca,geom.var = c("text"),geom.ind = c("text"), label="all",labelsize=3,col.ind=as.integer(b),repel=repel)+labs(title="")+theme(plot.background = element_rect(fill = 'white', colour = 'white')) + scale_color_gradientn(colors=rainbow(10,start=.65),guide="none")
+    plot<-fviz_pca_biplot(res.pca,geom.var = c("text"),geom.ind = c("text"), label="all",labelsize=3,col.ind=as.integer(b),col.var="black",repel=repel)+labs(title="")+theme(plot.background = element_rect(fill = 'white', colour = 'white')) + scale_color_gradientn(colors=rainbow(10,start=.65),guide="none")
 
     return(plot)
   }
