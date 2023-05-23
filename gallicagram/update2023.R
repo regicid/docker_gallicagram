0d7379936dc27983fd979192fbd67b0529bbb947
regicid <57593826+regicid@users.noreply.github.com>
2023-05-23 17:45:03 +0200

    nyt2023

---
+++ gallicagram/update2023.R
@@ -0,0 +1,7 @@
+library(rvest)
+library(stringr)
+a = read_html("https://www.nytimes.com/search?dropmab=false&query=have&endDate=20231231&startDate=20230101&types=article")
+count = str_split(html_text(a),'totalCount":')[[1]][2]
+count = str_split(count,",")[[1]][1]
+system("head -n -1 base_presse_annees_nyt.csv > temp.csv") ##Delete last line
+system(str_c("echo 173,",count, ",2023 >> temp.csv"))
--- gallicagram/update2023.py
+++ gallicagram/update2023.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-page = requests.get(f"https://www.nytimes.com/search?dropmab=false&query=have&sort=best&startDate=20230101&endDate=20231231&types=article")
+page = requests.get(f"https://www.nytimes.com/search?dropmab=false&types=article&query=have&endDate=20231231&startDate=20230101")
+soup = BeautifulSoup(page.content)
 a = int(str(soup.find_all("script")[8]).split('totalCount":')[-1].split(",")[0])
 os.system("head -n -1 base_presse_annees_nyt.csv > base_presse_annees_nyt.csv") ##Delete last line
 os.system(f'echo 173,{a},2023' >> base_presse_annees_nyt.csv)
