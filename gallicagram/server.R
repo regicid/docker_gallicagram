55fac38ab5812e87b8e58ddcbf50c91a14e25abb
benjyazoulay <benjyazoulay@gmail.com>
2023-08-02 15:32:45 +0200

    chat gpt

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -3969,8 +3969,8 @@ gptiseur<-function(input){
     encode = "json",
     body = list(
       model = "gpt-3.5-turbo-16k-0613",
-      messages = list(list(role = "system", content = "Tu es un robot historien. A partir d\'extraits de journaux tu dois trouver dans quel contexte historique ils ont été écrits. Tu dois expliquer en détail ce qu\'il s\'est passé durant la période que tu vas identifier. Sois précis, détaille le contexte historique, les évènements, les acteurs en présence, les enjeux politiques, sociaux ou économiques."),
-                      list(role = "user", content = str_c('Voilà des extraits de journaux parus durant la période ',gpt_start,' - ',gpt_end,' et contenant le mot ',gpt_word,'. Trouve dans quel contexte historique ce mot a été écrit. Explique en détail ce qu\'il s\'est passé durant cette période là. \n Extraits de journaux : \n <|startoftext|> ',gpt_context,' <|endoftext|>'))
+      messages = list(list(role = "system", content = "Tu es un robot historien. A partir d\'extraits de journaux tu dois trouver dans quel contexte historique ils ont été écrits. Tu dois expliquer en détail ce qu\'il s\'est passé durant la période que tu vas identifier. Sois précis, détaille le contexte historique, les évènements, les acteurs en présence, les enjeux politiques, sociaux ou économiques. Les extraits peuvent faire référence à plusieurs évènements, distingue les. Réponds de façon concise. Réponds par une liste à tiret pour distinguer ces évènements. Ne fais pas de conclusion."),
+                      list(role = "user", content = str_c('Voilà des extraits de journaux parus durant la période ',gpt_start,' - ',gpt_end,' et contenant le mot ',gpt_word,'. Trouve dans quel contexte historique ce mot a été écrit. Explique en détail ce qu\'il s\'est passé durant cette période là. 400 mots maximum, pas de phrase de conclusion. \n Extraits de journaux : \n <|startoftext|> ',gpt_context,' <|endoftext|>'))
                       ),
       max_tokens=600
     )
@@ -3978,6 +3978,9 @@ gptiseur<-function(input){
   chatGPT_answer <- content(response)$choices[[1]]$message$content
   chatGPT_answer <- stringr::str_trim(chatGPT_answer)
   cat(chatGPT_answer)
+  chatGPT_answer<-str_replace_all(chatGPT_answer,"\n","<br>")
+  chatGPT_answer<-HTML(chatGPT_answer)
+  
   
   return(chatGPT_answer)
 }
@@ -4072,7 +4075,7 @@ shinyServer(function(input, output,session){
     if(is.null(data$e)==F&(isolate(input$doc_type==1) | isolate(input$doc_type==2) | isolate(input$doc_type==56))){
     will<<-as.character(unlist(data$e$customdata))
     b=willisation(input,will)
-    output$gpt=renderText(gptiseur(input))
+    output$gpt=renderUI(HTML(gptiseur(input)))
       if(is.null(b)==F){
       wurl=str_replace(wurl,"https://gallica-grapher.ew.r.appspot.com/api/gallicaRecords","https://www.gallicagrapher.com/context")
       wurl=str_remove_all(wurl,"&row_split=true&cursor=0")
