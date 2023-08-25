7c71262bbc1957e669f823cc23fad279e97842af
regicid <benoit.de.courson@ens.fr>
2023-08-25 20:10:46 +0200

    ddb

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -1524,8 +1524,8 @@ ngramize<-function(input,nouvrequette,gallicagram,agregator){
             colnames(base)[1] = "date"
             #base$date = as.character(base$date)
           } else{
-          z = base$mois < 10
-          base$mois[z] = paste("0",base$mois[z],sep="")
+          zz = base$mois < 10
+          base$mois[zz] = paste("0",base$mois[z],sep="")
           base$date = paste(base$annee,base$mois,sep="/")
           base = base[c("n","date")]
           colnames(base)[1] = "base"}
@@ -3137,7 +3137,7 @@ get_data <- function(mot,from,to,resolution,doc_type,titres,input,cooccurrences,
           month2=1
         }
         search = paste(str_c("plainpagefulltext:",str_split(mot,"\\+")[[1]]),collapse = " OR ")
-        url = URLencode(glue("https://api.deutsche-digitale-bibliothek.de/search/index/newspaper-issues/select?q=paper_title:%22K%C3%B6lnische%20Zeitung.%201803-1945%22%20AND%20{search}%20AND%20publication_date:%5b{year}-{month}-01T00:00:00.000Z%20TO%20{year2}-{month2}-01T00:00:00.000Z%5d&rows=1000000000&fl="))
+        url = URLencode(glue("https://api.deutsche-digitale-bibliothek.de/search/index/newspaper-issues/select?q=paper_title:%22Deutsche%20allgemeine%20Zeitung%22%20AND%20{search}%20AND%20publication_date:%5b{year}-{month}-01T00:00:00.000Z%20TO%20{year2}-{month2}-01T00:00:00.000Z%5d&rows=1000000000&fl="))
         for(mot1 in str_split(mot,"\\+")[[1]]){
           url = glue("{url}termfreq(plainpagefulltext,{mot1}),")}
         url = URLencode(url)
