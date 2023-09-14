d9a573bcd0139bc8bac89bf6ab6caa389043ef4c
regicid <benoit.de.courson@ens.fr>
2023-09-14 14:55:24 +0200

    bla

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -4445,24 +4445,25 @@ shinyServer(function(input, output,session){
   observeEvent(input$language,{
     observeEvent(input$bibli,{
       if(input$language == 1 & input$bibli==0){
-        updateSelectInput(session,"doc_type", "Corpus",choices = list("Gallica-presse 1789-1950 / Le Monde 1945-2022" = 0,"Presse française / Gallica" = 1,"Livres / Gallica" = 2, "Livres+Presse / Gallica"=56,"Le Monde"=30,"L'Humanité (1904-1952)"=67,"Le Journal de Paris (1777-1827)"=69,"Le Temps (1861-1942)"=71,"Le Figaro (1854-1952)"=66,"Le Moniteur universel (1789-1901)"=70),selected = 1)
+        updateSelectInput(session,"doc_type", "Corpus",choices = list("Gallica-presse 1789-1950 / Le Monde 1945-2022" = 0,"Presse française / Gallica (~1789-1950)" = 1,"Livres / Gallica (~1600-1940)" = 2, "Livres+Presse / Gallica"=56,"Le Monde (1944-2022)"=30,"L'Humanité (1904-1952)"=67,"Le Journal de Paris (1777-1827)"=69,"Le Temps (1861-1942)"=71,"Le Figaro (1854-1952)"=66,"Le Moniteur universel (1789-1901)"=70),selected = 1)
       }
       else if(input$language == 1 & input$bibli==1){
-        updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse française / Gallica" = 1,"Recherche par titre de presse / Gallica" = 3, "Livres / Gallica" = 2, 
+        updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse française / Gallica (~1789-1950)" = 1,"Recherche par titre de presse / Gallica" = 3, "Livres / Gallica (~1600-1940)" = 2, 
                                                                       "Corpus personnalisé / Gallica"=4, "Livres+Presse / Gallica"=56,
                                                                       "Le Figaro (1854-1952)"=66,"L'Humanité (1904-1952)"=67,"Le Constitutionnel (1821-1913)"=68,
                                                                       "Le Journal de Paris (1777-1827)"=69,"Le Moniteur universel (1789-1901)"=70,
                                                                       "Le Temps (1861-1942)"=71,"Le Petit Journal (1863-1942)"=72,"Le Petit Parisien (1876-1944)"=73,"Le Journal Officiel (1869-1952)"=74,"Le Journal des Débats (1805-1944)"=75,"La Presse (1836-1930)"=76),selected = 1)
       }
       else if(input$language == 1 & input$bibli==2){
-        updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse suisse-romande / Bibliothèque nationale suisse"=15),selected = 15)
+        updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse suisse-romande / Bibliothèque nationale suisse"=15
+        ),selected = 15)
         #, "Presse wallonne / KBR"=13, "Presse québécoise / BAnQ"=28
       }
       else if(input$language == 1 & input$bibli==3){
         updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse Auvergne-Rhône-Alpes / Lectura"=17, "Presse du sillon lorrain / Limedia"=18, "Presse méridionale / Mémonum"=19, "Presse de Saint-Denis / Commun-Patrimoine"=20, "Presse de Brest / Yroise"=21, "Presse des Pyrénées / Pireneas"=22, "Presse toulousaine / Rosalis"=23, "Presse diplomatique / Bibliothèque diplomatique numérique"=24, "Presse francophone / RFN"=25, "Presse alsacienne / Numistral"=26, "Presse de Roubaix / BN-R"=27),selected = 17)
       }
       else if(input$language == 1 & input$bibli==4){
-        updateSelectInput(session,"doc_type", "Corpus",choices = list("Le Monde"=30, "Le Figaro"=31,"Presse française / MediaCloud"=50),selected = 30)#Le Marin 55
+        updateSelectInput(session,"doc_type", "Corpus",choices = list("Le Monde (1944-2022)"=30, "Le Figaro (2006-)"=31,"Presse française / MediaCloud"=50),selected = 30)#Le Marin 55
       }
       else if(input$language == 1 & input$bibli==5){
         updateSelectInput(session,"doc_type", "Corpus",choices = list("Isidore"=36,"Cairn.info"=32,"Theses.fr"=33,"Persée"=34),selected = 36)
