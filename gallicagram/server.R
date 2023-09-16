8256e1ecc001fb4006d607f72029509db42d236b
regicid <benoit.de.courson@ens.fr>
2023-09-16 12:55:16 +0200

    bla

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -847,9 +847,17 @@ SPlot <- function(data,input){
         theme_tufte()+ scale_color_manual(values=customPalette)+
         theme(plot.background = element_rect(fill = 'white', colour = 'white'),axis.line.x = element_line(colour = "black"),axis.line.y = element_line(colour = "black"),legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=8),legend.justification="left", legend.margin=margin(0,0,0,0),legend.box.margin=margin(-10,-10,0,-10),legend.key.height = unit(.5, 'lines'))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
     }
-    else{plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot))+geom_point(data=tableau, aes(x = date, y = ratio, group=mot,color=mot),size=.5, alpha=.5) + geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+xlab("")+ylab("")+
+    else{plot=ggplot(data=tableau, aes(x = date, y = loess, group=mot))+geom_point(data=tableau, aes(x = date, y = ratio, group=mot,color=mot),size=.5, alpha=.5) + geom_line(data = spline.d,aes(x=x,y=y,group=mot,color=mot),size=.7)+
+      xlab("Date")+ylab("Fréquence dans le corpus")+
       theme_tufte()+ scale_color_manual(values=customPalette)+
-      theme(plot.background = element_rect(fill = 'white', colour = 'white'),axis.line.x = element_line(colour = "black"),axis.line.y = element_line(colour = "black"),legend.title= element_blank(),legend.position="bottom", legend.box = "horizontal",legend.text = element_text(size=8),legend.justification="left", legend.margin=margin(0,0,0,0),legend.box.margin=margin(-10,-10,0,-10),legend.key.height = unit(.5, 'lines'))+guides(color=guide_legend(nrow=2, byrow=TRUE))}
+      theme(plot.background = element_rect(fill = 'white', colour = 'white'),
+            axis.line.x = element_line(colour = "black"),
+            axis.line.y = element_line(colour = "black"),
+            legend.title= element_blank(),legend.position="bottom", 
+            legend.box = "horizontal",legend.text = element_text(size=8),
+            legend.justification="left", legend.margin=margin(0,0,0,0),
+            legend.box.margin=margin(-10,-10,0,-10),legend.key.height = unit(.5, 'lines'))
+    +guides(color=guide_legend(nrow=2, byrow=TRUE))}
   }
   
   if(input$fraction==TRUE | input$delta==TRUE){
