0ca777906d1cfe2ca5e8cc96e4dcc50e657a916e
regicid <benoit.de.courson@ens.fr>
2023-10-18 11:23:42 +0200

    bla

--- gallicagram/server.R
+++ gallicagram/server.R
@@ -4475,14 +4475,14 @@ shinyServer(function(input, output,session){
   observeEvent(input$language,{
     observeEvent(input$bibli,{
       if(input$language == 1 & input$bibli==0){
-        updateSelectInput(session,"doc_type", "Corpus",choices = list("Gallica-presse 1789-1950 / Le Monde 1945-2022" = 0,"Presse française / Gallica (~1789-1950)" = 1,"Livres / Gallica (~1600-1940)" = 2, "Livres+Presse / Gallica"=56,"Le Monde (1944-2022)"=30,"Le Journal des Débats (1805-1944)"=75,"L'Humanité (1904-1952)"=67,"Le Petit Journal (1863-1942)"=72,"Le Petit Parisien (1876-1944)"=73,"Le Journal de Paris (1777-1827)"=69,"Le Temps (1861-1942)"=71,"Le Figaro (1854-1952)"=66,"Le Moniteur universel (1789-1901)"=70),selected = 1)
+        updateSelectInput(session,"doc_type", "Corpus",choices = list("Gallica-presse 1789-1950 / Le Monde 1945-2022" = 0,"Presse française / Gallica (~1789-1950)" = 1,"Livres / Gallica (~1600-1940)" = 2, "Livres+Presse / Gallica"=56,"Le Monde (1944-2022)"=30,"Le Journal des Débats (1789-1944)"=75,"L'Humanité (1904-1952)"=67,"Le Petit Journal (1863-1942)"=72,"Le Petit Parisien (1876-1944)"=73,"Le Journal de Paris (1777-1827)"=69,"Le Temps (1861-1942)"=71,"Le Figaro (1854-1952)"=66,"Le Moniteur universel (1789-1901)"=70),selected = 1)
       }
       else if(input$language == 1 & input$bibli==1){
         updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse française / Gallica (~1789-1950)" = 1,"Recherche par titre de presse / Gallica" = 3, "Livres / Gallica (~1600-1940)" = 2, 
                                                                       "Corpus personnalisé / Gallica"=4, "Livres+Presse / Gallica"=56,
                                                                       "Le Figaro (1854-1952)"=66,"L'Humanité (1904-1952)"=67,"Le Constitutionnel (1821-1913)"=68,
                                                                       "Le Journal de Paris (1777-1827)"=69,"Le Moniteur universel (1789-1901)"=70,
-                                                                      "Le Temps (1861-1942)"=71,"Le Petit Journal (1863-1942)"=72,"Le Petit Parisien (1876-1944)"=73,"Le Journal Officiel (1869-1952)"=74,"Le Journal des Débats (1805-1944)"=75,"La Presse (1836-1930)"=76),selected = 1)
+                                                                      "Le Temps (1861-1942)"=71,"Le Petit Journal (1863-1942)"=72,"Le Petit Parisien (1876-1944)"=73,"Le Journal Officiel (1869-1952)"=74,"Le Journal des Débats (1789-1944)"=75,"La Presse (1836-1930)"=76),selected = 1)
       }
       else if(input$language == 1 & input$bibli==2){
         updateSelectInput(session,"doc_type", "Corpus",choices = list("Presse suisse-romande / Bibliothèque nationale suisse"=15
